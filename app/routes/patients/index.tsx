f866bdfff351b384a8c7fd3a6de26aca4358886b
ethan.repp <ethan.repp@sygnomics.net>
2025-10-29 14:54:49 -0700

    nav links

---
+++ app/routes/patients/index.tsx
@@ -0,0 +1,9 @@
+import { ChartAreaInteractive } from "~/components/blocks/chart-area-interactive";
+import { DataTable } from "~/components/blocks/data-table";
+import { SectionCards } from "~/components/blocks/section-cards";
+
+import data from "~~/shad/dashboard/data.json";
+
+export default function Page() {
+	return <div>Here are some patients</div>;
+}
