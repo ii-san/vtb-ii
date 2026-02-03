3f25799b1e1ac311b4f3b0dec5d36c65ad1c850c
ethan.repp <ethan.repp@sygnomics.net>
2026-02-03 11:14:04 -0800

    nav links

--- app/components/blocks/app-sidebar.tsx
+++ app/components/blocks/app-sidebar.tsx
@@ -1,20 +1,4 @@
-import {
-	IconCamera,
-	IconChartBar,
-	IconDashboard,
-	IconDatabase,
-	IconFileAi,
-	IconFileDescription,
-	IconFileWord,
-	IconFolder,
-	IconHelp,
-	IconInnerShadowTop,
-	IconListDetails,
-	IconReport,
-	IconSearch,
-	IconSettings,
-	IconUsers,
-} from "@tabler/icons-react";
+import { IconVirus } from "@tabler/icons-react";
 import type * as React from "react";
 
 import { NavDocuments } from "~/components/blocks/nav-documents";
@@ -30,123 +14,7 @@ import {
 	SidebarMenuButton,
 	SidebarMenuItem,
 } from "~/components/ui/sidebar";
-
-const data = {
-	user: {
-		name: "Dr. Smith",
-		email: "dr@example.com",
-		avatar: "/avatars/1.jpg",
-	},
-	navMain: [
-		{
-			title: "Dashboard",
-			url: "#",
-			icon: IconDashboard,
-		},
-		{
-			title: "Lifecycle",
-			url: "#",
-			icon: IconListDetails,
-		},
-		{
-			title: "Analytics",
-			url: "#",
-			icon: IconChartBar,
-		},
-		{
-			title: "Projects",
-			url: "#",
-			icon: IconFolder,
-		},
-		{
-			title: "Team",
-			url: "#",
-			icon: IconUsers,
-		},
-	],
-	navClouds: [
-		{
-			title: "Capture",
-			icon: IconCamera,
-			isActive: true,
-			url: "#",
-			items: [
-				{
-					title: "Active Proposals",
-					url: "#",
-				},
-				{
-					title: "Archived",
-					url: "#",
-				},
-			],
-		},
-		{
-			title: "Proposal",
-			icon: IconFileDescription,
-			url: "#",
-			items: [
-				{
-					title: "Active Proposals",
-					url: "#",
-				},
-				{
-					title: "Archived",
-					url: "#",
-				},
-			],
-		},
-		{
-			title: "Prompts",
-			icon: IconFileAi,
-			url: "#",
-			items: [
-				{
-					title: "Active Proposals",
-					url: "#",
-				},
-				{
-					title: "Archived",
-					url: "#",
-				},
-			],
-		},
-	],
-	navSecondary: [
-		{
-			title: "Settings",
-			url: "#",
-			icon: IconSettings,
-		},
-		{
-			title: "Get Help",
-			url: "#",
-			icon: IconHelp,
-		},
-		{
-			title: "Search",
-			url: "#",
-			icon: IconSearch,
-		},
-	],
-	documents: [
-		{
-			name: "Data Library",
-			url: "#",
-			icon: IconDatabase,
-		},
-		{
-			name: "Reports",
-			url: "#",
-			icon: IconReport,
-		},
-		{
-			name: "Word Assistant",
-			url: "#",
-			icon: IconFileWord,
-		},
-	],
-};
+import { RouteNav } from "~/routes";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 	return (
@@ -158,21 +26,21 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 							asChild
 							className="data-[slot=sidebar-menu-button]:p-1.5!"
 						>
-							<a href="#">
-								<IconInnerShadowTop className="size-5!" />
-								<span className="text-base font-semibold">Acme Inc.</span>
+							<a href="/">
+								<IconVirus className="size-5!" />
+								<span className="text-base font-semibold">Sygnomics</span>
 							</a>
 						</SidebarMenuButton>
 					</SidebarMenuItem>
 				</SidebarMenu>
 			</SidebarHeader>
 			<SidebarContent>
-				<NavMain items={data.navMain} />
-				<NavDocuments items={data.documents} />
-				<NavSecondary items={data.navSecondary} className="mt-auto" />
+				<NavMain items={RouteNav.navMain} />
+				<NavDocuments items={RouteNav.documents} />
+				{/* <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
 			</SidebarContent>
 			<SidebarFooter>
-				<NavUser user={data.user} />
+				<NavUser user={RouteNav.user} />
 			</SidebarFooter>
 		</Sidebar>
 	);
--- app/routes.ts
+++ app/routes.ts
@@ -5,8 +5,88 @@ import {
 	route,
 } from "@react-router/dev/routes";
 
+import {
+	IconBrain,
+	IconCamera,
+	IconChartBar,
+	IconDashboard,
+	IconDatabase,
+	IconFileAi,
+	IconFileDescription,
+	IconFileWord,
+	IconFirstAidKit,
+	IconFolder,
+	IconHelp,
+	IconListDetails,
+	IconPills,
+	IconReport,
+	IconSearch,
+	IconSettings,
+	IconUsers,
+} from "@tabler/icons-react";
+
 export default [
 	layout("components/layouts/dashboard.tsx", [
 		index("routes/dashboard/index.tsx"),
+		route("patients", "routes/patients/index.tsx"),
 	]),
 ] satisfies RouteConfig;
+
+export const RouteNav = {
+	user: {
+		name: "Dr. Smith",
+		email: "dr@example.com",
+		avatar: "/avatars/1.jpg",
+	},
+	navMain: [
+		{
+			title: "Patients",
+			url: "/patients",
+			icon: IconUsers,
+		},
+		{
+			title: "Network Analysis",
+			url: "/#analysis",
+			icon: IconChartBar,
+		},
+		{
+			title: "Drug Predictions",
+			url: "/#predictions",
+			icon: IconPills,
+		},
+		// {
+		// 	title: "AI Recommendations",
+		// 	url: "/chat",
+		// 	icon: IconBrain,
+		// },
+	],
+	navSecondary: [
+		{
+			title: "Settings",
+			url: "#",
+			icon: IconSettings,
+		},
+		{
+			title: "Get Help",
+			url: "#",
+			icon: IconHelp,
+		},
+		{
+			title: "Search",
+			url: "#",
+			icon: IconSearch,
+		},
+	],
+	documents: [
+		{
+			name: "Patient Summary",
+			url: "#",
+			icon: IconReport,
+		},
+		{
+			name: "Treatement Outcomes",
+			url: "#",
+			icon: IconFirstAidKit,
+		},
+	],
+};
